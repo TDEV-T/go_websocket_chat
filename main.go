5a79e0ea5dc9889696016696960a70876dac20ff
TDEV-T <ktgz14211771329@gmail.com>
2024-07-29 16:28:53 +0700

    first

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	Rctx := context.Background()
+	ctx, cancel := context.WithCancel(Rctx)
+
+	defer cancel()
+
+	setupAPI(ctx)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Err: ", err)
+	}
+}
+
+func setupAPI(ctx context.Context) {
+
+	manager := NewManager()
+
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+
+	http.HandleFunc("/ws", manager.serverWS)
+}
